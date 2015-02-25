ce5224329f88ec715227ac735709e0176cc1c784
Diwaker Ghimire <idwaker@gmail.com>
2015-02-25 16:47:36 +0545

    added question for project 1 and removed unecessary cast

--- proj1.nim
+++ proj1.nim
@@ -1,4 +1,12 @@
 
+discard """
+If we list all the natural numbers below 10 that are multiples of 3 or 5, 
+we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+
+"""
+
 
 # variable to store final result
 var sum = 0
@@ -7,8 +15,7 @@ var sum = 0
 # countup includes stop value
 for i in countup(1, 999):
   # value should be divisible by either 3 or 5
-  if int(i mod 3) == 0 or int(i mod 5) == 0:
+  if i mod 3 == 0 or i mod 5 == 0:
     sum = sum + i
 
 echo($sum)
- 
