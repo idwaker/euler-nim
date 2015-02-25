1451ee4773d11e056f4f842664196ddbc390be21
Diwaker Ghimire <idwaker@gmail.com>
2015-02-25 17:52:24 +0545

    completed project 5 using blocks :)

---
+++ proj5.nim
@@ -0,0 +1,25 @@
+
+discard """
+
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 
+without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the 
+numbers from 1 to 20?
+
+"""
+
+var n = 20
+
+block main:
+  while true:
+    block inner:
+      for j in 1..20:
+        if n mod j == 0:
+          continue
+        else:
+          break inner
+      break main
+    n += 1
+
+echo($n)
