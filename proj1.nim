6eb4703de8b06eee86d29bebe7f9548f257da66c
Diwaker Ghimire <idwaker@gmail.com>
2015-02-25 12:38:06 +0545

    added comment

--- proj1.nim
+++ proj1.nim
@@ -1,8 +1,12 @@
 
 
-var sum: int = 0
+# variable to store final result
+var sum = 0
 
+# loop through 1 to 999
+# countup includes stop value
 for i in countup(1, 999):
+  # value should be divisible by either 3 or 5
   if int(i mod 3) == 0 or int(i mod 5) == 0:
     sum = sum + i
 
